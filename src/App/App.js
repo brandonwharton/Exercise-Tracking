9c8e5993679e43dc58759b2ea4fc901e5f007283
Brandon Wharton <whartonbm@gmail.com>
2021-08-26 14:40:03 -0500

    Made a Navbar component and a coverage test command, got rid of report web vitals

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import logo from '../logo.svg';
 import './App.css';
 import Header from './Header';
+import Navbar from '../Navbar/Navbar';
 
 function App() {
   return (
     <div className="App-header">
       <Header />
+      <Navbar />
     </div>
   );
 }
