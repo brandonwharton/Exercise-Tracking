8431619be42ac6b8d9fefe71fc5e173678408252
Brandon Wharton <whartonbm@gmail.com>
2021-09-02 16:12:08 -0500

    Built the POST route to add workouts

--- server/routes/workouts.router.js
+++ server/routes/workouts.router.js
@@ -6,8 +6,21 @@ const router = express.Router();
 // POST routes
 
 router.post('/', (req, res) => {
-    console.log('req.body', req.body);
-    res.sendStatus(200);
+    const queryText = `INSERT INTO "workouts" ("description", "activity_id", "date") 
+                       VALUES ($1, $2, $3);
+    `
+    // const values = [req.body.description, req.body.activity_id, req.body.date];
+    const {description, activity_id, date} = req.body;
+    const values = [description, activity_id, date];
+
+    pool.query(queryText, values)
+    .then(result => {
+        res.sendStatus(201);
+    })
+    .catch(error => {
+        console.log('Error with /api/workouts POST', error);
+        res.sendStatus(500);
+    })
 })
 
 
